0c782ee9f6d8aa60320adc9492ac830ae4c9a791
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2025-02-07 18:30:00 +0100

    var: small changes and tweaks

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,7 +1,7 @@
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
-		python = { "ruff" },
+		python = { "ruff", "black" },
 		javascript = { "prettierd", "prettier", stop_after_first = true },
 		typescript = { "prettierd", "prettier", stop_after_first = true },
 		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
@@ -13,5 +13,13 @@ require("conform").setup({
 		-- These options will be passed to conform.format()
 		timeout_ms = 500,
 		lsp_fallback = true,
+	},
+	formatters = {
+		ruff_format = {
+			prepend_args = {},
+		},
+		black = {
+			prepend_args = {},
+		},
 	},
 })
