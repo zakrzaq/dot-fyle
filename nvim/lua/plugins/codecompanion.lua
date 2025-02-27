a4038fbf5217fa20cc8029e65f708e6dac0dbeef
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2025-02-27 10:45:20 +0100

    feat: Add codecompanion plugin configuration

---
+++ nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,62 @@
+local M = {}
+
+function M.init()
+	-- write functionaly to be executed
+end
+
+function M.setup()
+	local installed, module = pcall(require, "codecompanion")
+	if not installed then
+		vim.notify("Plugin 'codecompanion' is not installed")
+		return
+	end
+
+	M.init()
+
+	module.setup({
+		enable_inline = false,
+		adapters = {
+			gemini = function()
+				return require("codecompanion.adapters").extend("gemini", {
+					env = {
+						api_key = "GEMINI_API_KEY",
+					},
+				})
+			end,
+			anthropic = function()
+				return require("codecompanion.adapters").extend("anthropic", {
+					env = {
+						api_key = "ANTHROPIC_KEY",
+					},
+				})
+			end,
+			llama3 = function()
+				return require("codecompanion.adapters").extend("ollama", {
+					name = "llama3",
+					-- Give this adapter a different name to differentiate it from the default ollama adapter
+					schema = {
+						model = {
+							default = "llama3:latest",
+						},
+						num_ctx = {
+							default = 16384,
+						},
+						num_predict = {
+							default = -1,
+						},
+					},
+				})
+			end,
+		},
+		strategies = {
+			chat = {
+				adapter = "gemini",
+			},
+			-- inline = {
+			-- 	adapter = "gemini",
+			-- },
+		},
+	})
+end
+
+return M
