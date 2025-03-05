812ce408e834d28998b25b74c9bb0870357574a8
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2025-03-05 21:05:01 +0100

    cleanup

---
+++ nvim/lua/plugins/kulala.lua
@@ -0,0 +1,68 @@
+local M = {}
+
+function M.init()
+	-- write functionaly to be executed
+end
+
+function M.setup()
+	local installed, module = pcall(require, "kulala")
+	if not installed then
+		vim.notify("Plugin 'kulala' is not installed")
+		return
+	end
+
+	M.init()
+
+	module.setup({
+		ui = {
+			display_mode = "split",
+			split_direction = "vertical",
+			default_view = "body",
+			winbar = true,
+			default_winbar_panes = { "body", "headers", "headers_body", "verbose" },
+			show_variable_info_text = false,
+			show_icons = "on_request",
+			icons = {
+				inlay = {
+					loading = "⏳",
+					done = "✅",
+					error = "❌",
+				},
+				lualine = "🐼",
+				textHighlight = "WarningMsg", -- highlight group for request elapsed time
+				lineHighlight = "Normal", -- highlight group for icons line highlight
+			},
+			show_request_summary = true,
+			summaryTextHighlight = "Special",
+		},
+		global_keymaps = {
+			["Send request"] = { -- sets global mapping
+				"<leader>Rs",
+				function()
+					require("kulala").run()
+				end,
+				mode = { "n", "v" }, -- optional mode, default is n
+				desc = "Send request", -- optional description, otherwise inferred from the key
+			},
+			["Send all requests"] = {
+				"<leader>Ra",
+				function()
+					require("kulala").run_all()
+				end,
+				mode = { "n", "v" },
+				ft = "http", -- sets mapping for *.http files only
+			},
+			["Replay the last request"] = {
+				"<leader>Rr",
+				function()
+					require("kulala").replay()
+				end,
+				ft = { "http", "rest" }, -- sets mapping for specified file types
+			},
+			["Find request"] = false, -- set to false to disable
+		},
+		kulala_keymaps = {},
+	})
+end
+
+return M
--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -1,114 +1,121 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  -- Core Utilities
-  { "tpope/vim-sleuth",          event = "VeryLazy" },
+	-- Core Utilities
+	{ "tpope/vim-sleuth", event = "VeryLazy" },
 
-  -- Theme Configuration
-  {
-    "AlexvZyl/nordic.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("nordic")
-    end,
-  },
+	-- Theme Configuration
+	{
+		"AlexvZyl/nordic.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("nordic")
+		end,
+	},
 
-  -- UI Components
-  { "akinsho/bufferline.nvim",   event = "VeryLazy",    opts = {} },
-  { "windwp/nvim-autopairs",     event = "InsertEnter", opts = {} },
-  { "folke/which-key.nvim",      event = "VeryLazy",    opts = {} },
-  { "nvim-lualine/lualine.nvim", event = "VeryLazy",    opts = {} },
-  { "numToStr/Comment.nvim",     event = "VeryLazy",    opts = {} },
-  { "folke/todo-comments.nvim",  event = "VeryLazy",    opts = {} },
+	-- UI Components
+	{ "akinsho/bufferline.nvim", event = "VeryLazy", opts = {} },
+	{ "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
+	{ "folke/which-key.nvim", event = "VeryLazy", opts = {} },
+	{ "nvim-lualine/lualine.nvim", event = "VeryLazy", opts = {} },
+	{ "numToStr/Comment.nvim", event = "VeryLazy", opts = {} },
+	{ "folke/todo-comments.nvim", event = "VeryLazy", opts = {} },
 
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-  },
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+	},
 
-  -- Text Manipulation
-  {
-    "kylechui/nvim-surround",
-    version = "*",
-    event = "VeryLazy",
-    opts = {},
-  },
+	-- Text Manipulation
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+		opts = {},
+	},
 
-  -- Syntax & Language
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
-    config = function()
-      pcall(require("nvim-treesitter.install").update({ with_sync = true }))
-    end,
-  },
+	-- Syntax & Language
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+		config = function()
+			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
+		end,
+	},
 
-  -- Git Integration
-  { "lewis6991/gitsigns.nvim", event = { "BufReadPre", "BufNewFile" }, opts = {} },
+	-- Git Integration
+	{ "lewis6991/gitsigns.nvim", event = { "BufReadPre", "BufNewFile" }, opts = {} },
 
-  -- LSP & Completion
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      { "j-hui/fidget.nvim", tag = "legacy", opts = {} },
-      "folke/neodev.nvim",
-    },
-  },
+	-- LSP & Completion
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			{ "j-hui/fidget.nvim", tag = "legacy", opts = {} },
+			"folke/neodev.nvim",
+		},
+	},
 
-  -- Formatting
-  { "stevearc/conform.nvim",   event = "BufReadPre",                   opts = {} },
+	-- Formatting
+	{ "stevearc/conform.nvim", event = "BufReadPre", opts = {} },
 
-  -- Note Taking
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*",
-    ft = "markdown",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
+	-- Note Taking
+	{
+		"epwalsh/obsidian.nvim",
+		version = "*",
+		ft = "markdown",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
-  -- Session Management
-  {
-    "rmagatti/auto-session",
-    event = "VimEnter",
-    dependencies = { "nvim-telescope/telescope.nvim" },
-    opts = {
-      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-    },
-  },
+	-- Session Management
+	{
+		"rmagatti/auto-session",
+		event = "VimEnter",
+		dependencies = { "nvim-telescope/telescope.nvim" },
+		opts = {
+			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+		},
+	},
 
-  -- AI Tools
+	-- REST
+	{
+		"mistweaverco/kulala.nvim",
+		keys = { "<leader>Rs", "<leader>Ra", "<leader>Ro" },
+		ft = { "http", "rest" },
+	},
 
-  { -- Codeium
-    "Exafunction/codeium.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-    },
-  },
+	-- AI Tools
+
+	{ -- Codeium
+		"Exafunction/codeium.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"hrsh7th/nvim-cmp",
+		},
+	},
 }, {})
