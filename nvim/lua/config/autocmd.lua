4e7a29ff631fa017b8abccc22ffc98e132464a88
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2025-03-07 14:21:13 +0100

    copilot binding

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -1,32 +1,32 @@
 -- VIRTUAL TEXT OFF
 vim.diagnostic.config({
-  float = {
-    source = true,
-    border = "rounded",
-    header = "",
-    prefix = "",
-    format = function(diagnostic)
-      return string.format("%s (%s)", diagnostic.message, diagnostic.source)
-    end,
-  },
-  virtual_text = false,
-  -- virtual_text = {
-  -- 	prefix = "●", -- Could be '●', '▎', 'x'
-  -- 	spacing = 4,
-  -- },
-  -- signs = true,
+	float = {
+		source = true,
+		border = "rounded",
+		header = "",
+		prefix = "",
+		format = function(diagnostic)
+			return string.format("%s (%s)", diagnostic.message, diagnostic.source)
+		end,
+	},
+	virtual_text = false,
+	-- virtual_text = {
+	-- 	prefix = "●", -- Could be '●', '▎', 'x'
+	-- 	spacing = 4,
+	-- },
+	-- signs = true,
 })
 
 -- DIAGNOSTICS ICONS
 local diagnostic_signs = {
-  { name = "DiagnosticSignError", text = "", texthl = "DiagnosticSignError" },
-  { name = "DiagnosticSignWarn", text = "", texthl = "DiagnosticSignWarn" },
-  { name = "DiagnosticSignInfo", text = "", texthl = "DiagnosticSignInfo" },
-  { name = "DiagnosticSignHint", text = "", texthl = "DiagnosticSignHint" },
+	{ name = "DiagnosticSignError", text = "", texthl = "DiagnosticSignError" },
+	{ name = "DiagnosticSignWarn", text = "", texthl = "DiagnosticSignWarn" },
+	{ name = "DiagnosticSignInfo", text = "", texthl = "DiagnosticSignInfo" },
+	{ name = "DiagnosticSignHint", text = "", texthl = "DiagnosticSignHint" },
 }
 
 for _, sign in ipairs(diagnostic_signs) do
-  vim.fn.sign_define(sign.name, { texthl = sign.texthl, text = sign.text, numhl = "" })
+	vim.fn.sign_define(sign.name, { texthl = sign.texthl, text = sign.text, numhl = "" })
 end
 
 -- SAVE FOLD ON FILE SAVE
@@ -39,5 +39,18 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, { command = "loadview", pattern =
 -- SPELL CHECKER
 local spell_dir = vim.fn.stdpath("config") .. "/spell"
 if vim.fn.isdirectory(spell_dir) == 0 then
-  vim.fn.mkdir(spell_dir, "p")
+	vim.fn.mkdir(spell_dir, "p")
 end
+
+-- COPILOT SYNTAX HIGHLIGHT
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "nordic",
+	-- group = ...,
+	callback = function()
+		vim.api.nvim_set_hl(0, "CopilotSuggestion", {
+			fg = "#555555",
+			ctermfg = 8,
+			force = true,
+		})
+	end,
+})
